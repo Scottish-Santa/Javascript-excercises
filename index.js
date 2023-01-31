2d1f7572cf3cd80bb332d0c56ee6f02ea9502d8d
Brendan Fraser <brendofraser@gmail.com>
2023-01-31 01:06:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create variables for each ingredient
+let spaghetti = 400;
+let guanciale = 250;
+let eggYolks = 6;
+let pecorino = 50;
+let blackPepper = 4;
+
+// Create a variable for the final dish
+let carbonara = spaghetti + guanciale + eggYolks + pecorino + blackPepper;
+
+// Log the total ingredients in the dish
+console.log(`The total ingredients used in the carbonara dish are: ${carbonara}g`);
+
